c855ddbe1f148a1ff927ef5b1f251ce3a13edf33
vicenteec <vicenteec@mail.com>
2023-04-20 19:22:19 +0200

    Inicio practica

---
+++ src/main/java/es/mdef/gestionPreguntas/GestionPreguntasApplication.java
@@ -0,0 +1,13 @@
+package es.mdef.gestionPreguntas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPreguntasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPreguntasApplication.class, args);
+	}
+
+}
