daf5eeea2407f16fddd4b4e284e9afa8aa663d53
vicenteec <vicenteec@mail.com>
2023-04-22 00:07:30 +0200

    Cambios @Enumerated(EnumType.STRING)

--- src/main/java/es/mdef/gestionPreguntas/entidades/NoAdministrador.java
+++ src/main/java/es/mdef/gestionPreguntas/entidades/NoAdministrador.java
@@ -2,46 +2,49 @@ package es.mdef.gestionPreguntas.entidades;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
 @Entity
 @DiscriminatorValue("N")
 public class NoAdministrador extends Usuario {
-	public static enum Departamento {
-		EMIES,
-		CCESP
-	}
-	
-	public static enum Tipo {
-		alumno,
-		docente,
-		administracion
-	}
-	
-	private Departamento departamento;
-	private Tipo tipo;
-	
-	public Departamento getDepartamento() {
-		return departamento;
-	}
-	public Tipo getTipo() {
-		return tipo;
-	}
-	public void setDepartamento(Departamento departamento) {
-		this.departamento = departamento;
-	}
-	public void setTipo(Tipo tipo) {
-		this.tipo = tipo;
-	}
-	
-	@Override
-	public Role getRole() {
-		// TODO Auto-generated method stub
-		return Role.NoAdministrador;
-	}
-	@Override
-	public String toString() {
-		return "NoAdministrador [departamento=" + departamento + ", tipo=" + tipo + "]";
-	}
-	
-	
+
+    public static enum Departamento {
+        EMIES,
+        CCESP
+    }
+
+    public static enum Tipo {
+        alumno,
+        docente,
+        administracion
+    }
+
+    @Enumerated(EnumType.STRING)
+    private Departamento departamento;
+
+    @Enumerated(EnumType.STRING)
+    private Tipo tipo;
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+    public Tipo getTipo() {
+        return tipo;
+    }
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public Role getRole() {
+        return Role.NoAdministrador;
+    }
+    @Override
+    public String toString() {
+        return "NoAdministrador [departamento=" + departamento + ", tipo=" + tipo + "]";
+    }
 }
--- src/main/java/es/mdef/gestionPreguntas/entidades/Usuario.java
+++ src/main/java/es/mdef/gestionPreguntas/entidades/Usuario.java
@@ -7,6 +7,8 @@ import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,58 +16,59 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 
-@Entity                                               // @Entity y @Table: Estas anotaciones indican que esta clase es una entidad de base de datos y debe mapearse a una tabla llamada "USUARIOS" en la base de datos.
+@Entity
 @Table(name="USUARIOS")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) 
 @DiscriminatorColumn(name="tipo_role", discriminatorType = DiscriminatorType.CHAR)
 @DiscriminatorValue("null")
 @JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"})
 public class Usuario {
-	public static enum Role {
-		Administrador,
-		NoAdministrador
-	}
 
-	@Id                                               // La anotación @Id indica que el campo "id" es la clave primaria de la tabla de usuarios. 
-	@GeneratedValue(strategy=GenerationType.IDENTITY) // La anotación @GeneratedValue indica que el valor de "id" se generará automáticamente al insertar un nuevo registro en la base de datos.
-	@JsonIgnore                                       // indica que el campo "id" no debe incluirse en la serialización JSON. Esto podría ser útil si no deseas que el identificador se envíe al cliente cuando conviertes la entidad en una respuesta JSON.
-	private Long id;
-	private String nombre;
-	private String nombreUsuario;
-	private String contrasena;
+    public static enum Role {
+        Administrador,
+        NoAdministrador
+    }
 
-	
-	public Long getId() {
-		return id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public String getNombreUsuario() {
-		return nombreUsuario;
-	}
-	public String getContrasena() {
-		return contrasena;
-	}
-	public Role getRole() {
-		return null;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public void setNombreUsuario(String nombreUsuario) {
-		this.nombreUsuario = nombreUsuario;
-	}
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
-	}
-	@Override
-	public String toString() {
-		return "Usuario [id=" + id + ", nombre=" + nombre + ", nombreUsuario=" + nombreUsuario + ", contrasena="
-				+ contrasena + "]";
-	}
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long id;
+    private String nombre;
+    private String nombreUsuario;
+    private String contrasena;
 
+    public Long getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+    public String getContrasena() {
+        return contrasena;
+    }
+    
+    @Enumerated(EnumType.STRING)
+    public Role getRole() {
+        return null;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    @Override
+    public String toString() {
+        return "Usuario [id=" + id + ", nombre=" + nombre + ", nombreUsuario=" + nombreUsuario + ", contrasena="
+                + contrasena + "]";
+    }
 }
